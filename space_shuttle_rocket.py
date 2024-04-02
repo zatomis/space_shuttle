fd358eb070f220c0b82b7df53bac202478690794
zatomis <zatomis@list.ru>
2024-04-02 13:47:01 +0300

    Асинхронная звезда (мигание)

---
+++ space_shuttle_rocket.py
@@ -0,0 +1,51 @@
+import time
+import asyncio
+import curses
+
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+
+def draw(canvas):
+    canvas.border()
+    curses.curs_set(False)
+    row, column = (5, 20)
+    coroutine1 = blink(canvas, row, column, symbol='*')
+    coroutine2 = blink(canvas, row+2, column, symbol='*')
+    coroutine3 = blink(canvas, row+4, column, symbol='*')
+    coroutine4 = blink(canvas, row+6, column, symbol='*')
+    coroutine5 = blink(canvas, row+8, column, symbol='*')
+    coroutines = [coroutine1,coroutine2,coroutine3,coroutine4,coroutine5]
+    while True:
+        try:
+            coroutine.send(None)
+            time.sleep(0.5)
+            canvas.refresh()
+
+        except StopIteration:
+            break
+
+
+
+if __name__ == '__main__':
+    curses.update_lines_cols()
+    curses.wrapper(draw)
+
+    # coroutine = count_to_three(10)
+    # while True:
+    #     try:
+    #         coroutine.send(None)
+    #     except StopIteration:
+    #         break
+
